3ba0572043435eaf2f31d4ad399e742b32d398b9
Sebilebi <ivanbollz25@gmail.com>
2025-01-23 14:25:51 +0100

    Añadir Models: MinecraftContent y ItemList

---
+++ Dravion/Models/ItemList.cs
@@ -0,0 +1,22 @@
+﻿namespace Dravion.Models
+{
+    public class ItemList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SelectedVersion { get; set; }
+        public Dictionary<string, List<MinecraftContent>> Items { get; set; }
+
+        public ItemList()
+        {
+            Items = new Dictionary<string, List<MinecraftContent>>
+            {
+                ["mods"] = new List<MinecraftContent>(),
+                ["plugins"] = new List<MinecraftContent>(),
+                ["shaders"] = new List<MinecraftContent>(),
+                ["resourcepacks"] = new List<MinecraftContent>(),
+                ["worlds"] = new List<MinecraftContent>()
+            };
+        }
+    }
+}
---
+++ Dravion/Models/MinecraftContent.cs
@@ -0,0 +1,13 @@
+﻿namespace Dravion.Models
+{
+    public class MinecraftContent
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public List<string> Versions { get; set; }
+        public string Downloads { get; set; }
+        public int Id { get; set; }
+        public string Summary { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
